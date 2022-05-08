b1f6c8c7b358d70eb9fba9357f7dfbd1b041460f
HarrySchmidt98 <harry.schmidt@my.jcu.edu.au>
2022-05-08 12:42:46 +1000

    created basic theme using underscores
    created new base theme using underscores.
    
    footer to be populated and slight changes yet to be made

---
+++ wp-content/themes/_s-master/header.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	
+
+	<header id="masthead" class="site-header">
+		<div class="site-logo">
+			<a id="logo" href="<?php echo home_url(); ?>" title="<?php _e('Home', 'bigblank'); ?>" rel="home">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/tjcLogo.jpg" alt="<?php bloginfo('name'); ?> logo"/>
+                <span class="screen-reader-text"><?php bloginfo('name'); ?></span>
+            </a>
+		</div>
+		
+
+		<nav id="site-navigation" class="main-navigation">
+			<div class="nav-wrapper">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+			?>
+
+			</div>
+			
+		</nav><!-- #site-navigation -->
+
+		<div class="header-banner">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/bannerImg1.jpg" alt="<?php bloginfo('name'); ?> logo"/>
+
+		</div>
+	</header><!-- #masthead -->
